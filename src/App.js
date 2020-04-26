9e7f5fd02afb3f3f1aa204389e85e6c798bdda19
aldrin163 <aldrinjenson@gmail.com>
2020-04-26 11:04:54 +0530

    Updating app with a shopPage, collectionPreview and CollectionItem. Also added the imagesgit add -A

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React from 'react';
-import './App.css';
-import Homepage from './pages/homepage.component';
-import { Switch, Route } from 'react-router-dom';
-
-const HatsPage = () =>{
-  return(
-    <h1>HatsPage</h1>
-  )
-}
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+import Homepage from "./pages/homepge/homepage.component";
 
 function App() {
   return (
-    <div>      
+    <div>
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route exact path="/" component={ Homepage } />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
