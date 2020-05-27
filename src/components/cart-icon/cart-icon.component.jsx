ba930b993b384c3681f07411eb3bd1b3a99f3f00
aldrin163 <aldrinjenson@gmail.com>
2020-05-28 04:34:45 +0530

    upating application with reselect. Replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from 'react' 
 import './cart-icon.styles.scss'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import { connect } from 'react-redux'
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
 
-const CartIcon = ({toggleCartHidden}) => (
-   <div className="cart-icon" onClick={toggleCartHidden} >
-      <ShoppingIcon className='shopping-icon' />
-      <span className="item-count">0</span>
-   </div>
-)
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
+   console.log('being called') // this will now be called only when the component rerenders
+   return (
+      <div className="cart-icon" onClick={toggleCartHidden} >
+         <ShoppingIcon className='shopping-icon' />
+         <span className="item-count">{itemCount}</span>
+      </div>
+   )
+}
+const mapStateToProps = state => {
+   return ({
+      // without memoization:
+      //  itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+      itemCount: selectCartItemsCount(state) // with memoization
+   })
+}
+// mapStateToProps will cause the whole component to rerender whenever any part of the global state changes
+// However, this will cause performance issues and as a result we opt for memoixzation methouds using the reselct library.
+// the selectCartItemsCount checks whether at the time of rerendering if the value obtained before and after state change are same and if it is same, then a new object is not returned like shown above and hence the component is not rerendered
 
 const mapDispatchToProps = dispatch => ({
    toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,47 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart; //input selector
+// input selector gets the state and almost always pulls one layer deep into the whole reducer state
+// Follow the naming convention
+// like this:
+// const selectUser = state.user
+
+// output selector
+export const selectCartItems = createSelector(
+  [selectCart], //first argument is an array of input selectors and the second argument is going to be a function which returns the value we want from the selector
+
+  (cart) => cart.cartItems // now this has become a memoized selector
+);
+
+// now considering the above as another input selector for this one
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      // here we are using the cartItem, which we would have got from using the above selector as an input selector
+      (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+    )
+);
+
+// const selectCart = state.cart
+// const selectUser = state.user
+
+// export const selectCartItems = createSelector(
+//   [selectCart, selectUser],
+//   (cart,user) => some_function_calculation
+// )
+
+// import { createSelector } from "reselect";
+
+// const selectCart = (state) => state.cart;
+
+// export const selectCartItems = createSelector(
+//   [selectCart],
+//   (cart) => cart.cartItems
+// );
+
+// export const selectCartItemsCount = createSelector(
+//   [selectCartItems],
+//   (cartItems) =>
+//     cartItems.reduce((accumulator, el) => accumulator + el.quantity, 0)
+// );
