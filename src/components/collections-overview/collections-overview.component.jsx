0d5b52787a803e1f7a32a21a59351e6ad199206b
aldrin163 <aldrinjenson@gmail.com>
2020-06-06 19:36:32 +0530

    Fixed up the error for the shop tab not rendering due to the shop data being converted to a hash table instead of an array

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,7 +3,7 @@ import './collections-overview.styles.scss'
 import CollectionPreview from '../preview-collection/collection-preview.component'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
 
 const CollectionOverview = ({collections}) => {
    return (
@@ -16,6 +16,6 @@ const CollectionOverview = ({collections}) => {
 }
 
 const mapStateToProps = createStructuredSelector({
-   collections: selectCollections
+   collections: selectCollectionsForPreview
 })
 export default  connect(mapStateToProps)(CollectionOverview)
